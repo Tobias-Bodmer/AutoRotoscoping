25e8178b0376377df190d1922c904a96775d85a9
Tobias Bodmer <48796225+Tobias-Bodmer@users.noreply.github.com>
2020-12-15 17:04:03 +0100

    firstSteps

---
+++ code/autoroto.py
@@ -0,0 +1,22 @@
+import FindMattes as fm
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import simpledialog as sd
+from os import listdir
+
+# Get the file directory
+root = tk.Tk()
+root.withdraw()
+
+directoryName = filedialog.askdirectory(parent=root, initialdir="/", title = 'Please select a directory')
+matteHeight = sd.askinteger("Set the matte size", "Output matte vertical resolution (256 recommended for quick tests)?", parent=root,initialvalue=256)
+
+listOfFiles = listdir(directoryName)
+
+for currentFile in listOfFiles:
+    sourceFile = directoryName + "/" + currentFile
+    mainNameEnd = currentFile.find('.')
+    nameForMatte = currentFile[:mainNameEnd] + "_matte" + currentFile[mainNameEnd:]
+    fullPathMatteName = directoryName + "/" + nameForMatte
+    fm.createMatte(sourceFile, fullPathMatteName, matteHeight)
+    print("Just created: " + nameForMatte)
