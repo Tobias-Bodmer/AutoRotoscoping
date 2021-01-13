69fe37e40fad15853af639e1bd105894f46ac01a
nikolagorgiev <48891699+nikolagorgiev@users.noreply.github.com>
2021-01-13 16:07:44 +0100

    Interface

--- code/interface.py
+++ code/interface.py
@@ -1,3 +1,13 @@
-from tkinter import filedialog
+import Export
+import Import
+import tkinter as tk
+from tkinter import messagebox as mb
 
-bla = filedialog.askdirectory()
+def call():
+    result = mb.askquestion('Maskenabfrage', 'Exestiert bereits eine Maske?')
+    if result == 'yes' :
+        Export
+    else :
+        Import
+
+call()
